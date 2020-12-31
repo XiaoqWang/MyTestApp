149f105cda8e830a22e11add0e4dc9338d18985e
XiaoqWang <xiaoqi9302@163.com>
2020-12-31 15:28:26 +0800

    Initial Commit

---
+++ MyTestApp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MyTestApp
+//
+//  Created by xiaoqi on 2020/12/31.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyTestApp/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  MyTestApp
+//
+//  Created by xiaoqi on 2020/12/31.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
