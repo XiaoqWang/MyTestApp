00c422f67cbc27a56056470f7baef455d5345e45
XiaoqWang <xiaoqi9302@163.com>
2020-12-31 15:31:23 +0800

    创建view

--- MyTestApp/ViewController.m
+++ MyTestApp/ViewController.m
@@ -16,6 +16,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(10, 100, 100, 100)];
+    view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view];
 }
 
 
